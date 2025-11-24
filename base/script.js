11c96901b90bc0106cfac932648cea70a0e35f59
Alone <alone85160@gmail.com>
2025-11-24 13:57:12 +0100

    Second commit, apprentissage en cours.

---
+++ base/script.js
@@ -0,0 +1,50 @@
+
+
+
+
+const name = "Martin";
+console.log(name)
+
+let AnneeNaissance = "2000";
+    AnneeNaissance = 2025-2000;
+console.log(AnneeNaissance)
+
+console.log("Bonjour Mr",name,"Vous Avez", AnneeNaissance, "Ans")
+
+
+// Exercice 2.
+
+
+let note_maths = 15;
+let note_francais = 12;
+let note_HG = 9;
+let moyenne = (15+12+9) /3;
+console.log("La moyenne est de",moyenne,"/20")
+
+// Exercice 3
+
+let sexe = 1;
+
+if (sexe === 1){
+    console.log("Vous etes un homme")
+
+}
+else if (sexe === 2){
+    console.log("Vous etes une femme")
+
+}
+
+// Exercice 4 
+
+let hour = 12 
+
+if (hour >= 6 && hour < 12){
+    console.log("Nous sommes le matin")
+}
+
+if (hour >= 12 && hour < 20) 
+    console.log("Nous sommes l'apres-midi")
+
+if (hour >= 20 && hour < 6 ) 
+    console.log("Nous sommes le soir")
+
