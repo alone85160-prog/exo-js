69840b60a531877ebcdafce5f170f7b1aea96cb6
Alone <alone85160@gmail.com>
2025-11-25 09:10:40 +0100

    premiere etape rendu exo 3

---
+++ base exo 2/script.js
@@ -0,0 +1,22 @@
+
+// Exercice 1
+
+let name = "jean";
+let age = 25;
+age = 30;
+console.log(name);
+console.log(age);
+
+// Exercice 2 
+
+let a = 10;
+let b = 20;
+console.log(a+b)
+console.log(a*b)
+
+// Exercice 3
+
+let body = prompt ("Veuillez entrer un nombre")
+if(body > 10){
+        console.log("INFERIEUR")
+}
